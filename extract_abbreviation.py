4fd0a5eea7c9e3ea90b840ec11426ee59ec97116
Ryuichi Tachibana <ryuu0614@gmail.com>
2014-06-26 15:48:57 +0900

    括弧表現のある文をリストに格納し、そのリストを表示するようにしました

--- extract_abbreviation.py
+++ extract_abbreviation.py
@@ -3,7 +3,14 @@
 
 import sys
 import string
+import re
 
-rf = open('resource_data.txt').read()
+#一文ある毎に改行されているテキストを想定している
+file_object = open('resource_data.txt')
+abbreviation_list = []
+for line in file_object:
+    match = re.search('(.+?)')
+    if match is not None:
+        abbreviation_list.append(line)
 
-print rf
+print abbreviation_list
