35222717e2ced3aeaa8b09fc5121c7c0bcdaf31f
Ryuichi Tachibana <ryuu0614@gmail.com>
2014-06-26 17:04:02 +0900

    出力結果をテキストとして出力させるようにしました

--- extract_abbreviation.py
+++ extract_abbreviation.py
@@ -6,10 +6,14 @@ import string
 import re
 
 #一文ある毎に改行されているテキストを想定している
-file_object = open('sample.txt')
+file_object = open('sample.txt',"r")
+wf = open('output_file.txt',"w")
+#wf2 = open('output_file2.txt',"w")
 abbreviation_list = []
 for line in file_object:
     match = re.search('(.+?)', line)
     if match is not None:
         abbreviation_list.append(line)
-        print line
+#        wf2.write(line)
+for i in range(0,len(abbreviation_list)):
+    wf.write(abbreviation_list[i])
