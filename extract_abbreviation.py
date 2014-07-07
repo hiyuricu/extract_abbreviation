bd54d98b1d4007975b1feb6d15540e94cd3c7dbf
Ryuichi Tachibana <ryuu0614@gmail.com>
2014-07-07 19:05:16 +0900

    其々の関数を別々に定義し、略語を含む文章を分かち書きする関数を追加しました

--- extract_abbreviation.py
+++ extract_abbreviation.py
@@ -6,8 +6,13 @@ import string
 import re
 import MeCab
 
-#一文ある毎に改行されているテキストを想定している
+#一文ある毎に改行されているテキストが引数にあると想定している
 def main(read_file):
+    output_abbreviation(read_file)
+    setense_wakati()
+
+#略語と略語を含む文を出力する関数
+def output_abbreviation(read_file):
     file_object = open(read_file,"r")
     wf = open('output_file.txt',"w")
     for line in file_object:
@@ -16,6 +21,13 @@ def main(read_file):
             wf.write("%s\t%s\n" % (line,m.group(1)))
     file_object.close()
     wf.close()
+
+#文章を分かち書きする関数
+def setense_wakati():
+    rf = open('output_file.txt',"r").read()
+    tagger = MeCab.Tagger('-Owakati')
+    result = tagger.parse(rf)
+    print result
 
 if __name__ == "__main__":
     main(sys.argv[1])
