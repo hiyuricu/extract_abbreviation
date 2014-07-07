b8a8d5dc22b64b8f203802c0d66407942d3aacd0
Ryuichi Tachibana <ryuu0614@gmail.com>
2014-07-07 13:53:23 +0900

    略語と略語を含む文のtsvファイルを出力し、一つの文に複数の略語がある場合にも対応しました

--- extract_abbreviation.py
+++ extract_abbreviation.py
@@ -8,12 +8,7 @@ import re
 #一文ある毎に改行されているテキストを想定している
 file_object = open('sample.txt',"r")
 wf = open('output_file.txt',"w")
-#wf2 = open('output_file2.txt',"w")
-abbreviation_list = []
 for line in file_object:
-    if re.search(r'\(.+?\)', line) is not None:
-        print re.search(r'\((.+?)\)', line).group(1)
-        abbreviation_list.append(line)
-#        wf2.write(line)
-for i in range(0,len(abbreviation_list)):
-    wf.write(abbreviation_list[i])
+    line = line.strip()
+    for m in re.finditer(r'\((.+?)\)', line):
+        wf.write("%s\t%s\n" % (line,m.group(1)))
