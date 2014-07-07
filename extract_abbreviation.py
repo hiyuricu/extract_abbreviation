bb718d0d373a034bfc1f7e5e44f8353013693246
Ryuichi Tachibana <ryuu0614@gmail.com>
2014-07-07 18:04:52 +0900

    スクリプトファイルとして実行できるようにしました

--- extract_abbreviation.py
+++ extract_abbreviation.py
@@ -4,11 +4,18 @@
 import sys
 import string
 import re
+import MeCab
 
 #一文ある毎に改行されているテキストを想定している
-file_object = open('sample.txt',"r")
-wf = open('output_file.txt',"w")
-for line in file_object:
-    line = line.strip()
-    for m in re.finditer(r'\((.+?)\)', line):
-        wf.write("%s\t%s\n" % (line,m.group(1)))
+def main(read_file):
+    file_object = open(read_file,"r")
+    wf = open('output_file.txt',"w")
+    for line in file_object:
+        line = line.strip()
+        for m in re.finditer(r'\((.+?)\)', line):
+            wf.write("%s\t%s\n" % (line,m.group(1)))
+    file_object.close()
+    wf.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1])
